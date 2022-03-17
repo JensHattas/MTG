3c5785431b245f8ec56288773fa8f324121e0233
Jens Hattas <jens.hattas@gmail.com>
2022-03-17 15:26:39 +0100

    begin api

--- js/program.js
+++ js/program.js
@@ -1,42 +1,41 @@
 const CardList = document.getElementById('CardList');
 const searchbar = document.getElementById('Searchbar');
 let MTGCards = [];
-console.log("cringe");
 
 searchbar.addEventListener('keyup', (e) => {
-    const searchString = e.target.value;
-    //console.log(e.target.value);
+    const searchString = e.target.value.toLowerCase();
+    //if searchString is H -> h
+    //if searchString is h -> h
+    //convert de naam eerst naad lowercase, vergelijk dan...
     //!!!!ONDERSTE IS NOG FOUT NIET AANRAKEN PLZ!!!!!!!
     const filteredCards = MTGCards.cards.filter((cards) => {
-        return (cards.name.includes(searchString) || 
-                cards.names.includes(searchString)
-            );
+        return cards.name.toLowerCase().includes(searchString);
     });
     console.log(filteredCards);
+    displayCards(filteredCards);
 });
+
 const loadCards = async () => {
     try {
         const res = await fetch('https://api.magicthegathering.io/v1/cards');
         MTGCards = await res.json();
-        console.log(MTGCards.cards[0]);
-        //displayCards(MTGCards);
+        //console.log(MTGCards.cards[0]);
+        displayCards(MTGCards.cards);
     } catch (err) {
         console.error(err);
     }
 };
-
-/*const displayCards = (cards) => {
+loadCards();
+const displayCards = (cards) => {
     const htmlString = cards
         .map((cards) => {
             return `
             <li class="character">
-                <h2>${card.name}</h2>
-                <img src="${card.image}"></img>
+                <h2>${cards.name}</h2>
+                <img src="${cards.imageUrl}"></img>
             </li>
         `;
         })
         .join('');
     CardList.innerHTML = htmlString;
 };
-*/
-loadCards();
